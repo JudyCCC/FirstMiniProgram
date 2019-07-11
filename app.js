450698d0f481c56e1ab248689a993d03f036ac0f
JudyCCC <703433699@qq.com>
2019-07-11 22:00:05 +0800

    cjy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ App({
   globalData:{
     g_isPlayingMusic: false,    // 是否有音乐播放
     g_currentMusicPostId:null,    // 哪一个音乐正在被播放
+    doubanBase: 'https://douban.uieee.com',   // 豆瓣地址
   }
 })
 // 先onLaunch再onShow
--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,3 +1,62 @@
+var app = getApp();
 Page({
-  
+
+  data:{
+    inTheaters: {},
+    comingSoon: {},
+    top250: {}
+  },
+
+  onLoad: function(event){
+    var inTheatersUrl = app.globalData.doubanBase +  '/v2/movie/in_theaters' + '?start=0&count=3';
+    var comingSoonUrl = app.globalData.doubanBase + '/v2/movie/coming_soon' + '?start=0&count=3';
+    var top250Url = app.globalData.doubanBase + '/v2/movie/top250' + '?start=0&count=3';
+
+    this.getMovieListData(inTheatersUrl, 'inTheaters');
+    this.getMovieListData(comingSoonUrl, 'comingSoon');
+    this.getMovieListData(top250Url, 'top250');
+  },
+
+  getMovieListData: function (url, settedKey){
+    var that = this;
+    wx.request({
+      url: url,
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/xml'
+      },
+      success: function (res) {
+        console.log(res)
+        that.processDoubanData(res.data, settedKey)
+      },
+      fail: function () { // 接口请求未成功 
+        console.log('failed')
+      }, 
+    })
+  },
+
+  processDoubanData: function (moviesDouban, settedKey){
+    var movies = [];
+    for(var idx in moviesDouban.subjects){
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if(title.length >= 6){
+        title = title.substring(0, 6) + '...';
+      }
+      var temp = {
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
+      }
+      movies.push(temp)
+    }
+    var readyData = {};
+    readyData[settedKey] = {
+      movies: movies
+    };
+    console.log(readyData)
+    this.setData(readyData)
+  },
+
 })
